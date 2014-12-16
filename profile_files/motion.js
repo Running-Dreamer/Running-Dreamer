059e4a6ca554fb6fb10d9b460503edc18e523952
Bonerchick <jerry82422@hotmail.com>
2014-12-16 20:30:25 +0800

    add files

---
+++ profile_files/motion.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+function mousein() {
+				document.getElementById('info').style.display = "block";
+			};
+function mouseout() {
+				document.getElementById('info').style.display = "none";
+			};
