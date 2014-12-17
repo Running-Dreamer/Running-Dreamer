210388e9b409fdfe7781512861ef18053959d6c5
jimmy <chioulf2@yahoo.com.tw>
2014-12-17 21:23:42 +0800

    全部砍掉重練  但也只能應急用= =  css那些太亂

--- others_files/motion.js
+++
@@ -1,7 +0,0 @@
-// JavaScript Document
-function mousein() {
-				document.getElementById('info').style.display = "block";
-			};
-function mouseout() {
-				document.getElementById('info').style.display = "none";
-			};
