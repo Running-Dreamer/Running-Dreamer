4c4444afbb541e69a0bba763e903a7dff7783e1b
Bonerchick <jerry82422@gmail.com>
2014-12-29 01:30:03 +0800

    修一下list(不知道有沒有比較好)，加入動態產生paper(需調整)

--- public/page/list/list.js
+++ public/page/list/list.js
@@ -2,6 +2,13 @@
 	fn.start = start;
 	
 	function start() {
-		
+		var $paperArea = $('.paper-area');
+		var i, max = 7;
+		for(i = 0; i < max; i += 1) {
+			var $paper = $('<div class="paper"></div>');
+			
+			$paper.css({'left': Math.random()*80+"%", 'top': Math.random()*80+"%"});
+			$paperArea.append($paper);
+		}
 	}
 })()
