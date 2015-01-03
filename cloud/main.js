fec3e33b4e1380c1809825e135fc5cb503ccf409
jimmy <chioulf2@yahoo.com.tw>
2015-01-03 21:00:26 +0800

    新增夢想時會在user建立起關聯 / parse db 結構更動

--- cloud/main.js
+++ cloud/main.js
@@ -27,6 +27,35 @@ Parse.Cloud.define("adminNewDream", function (request, response) {
 	dream.set("photo",photo);
 //	dream.set("type",type);
 	dream.save().then(function(){
-		response.success("dream saved!");
+		//response.success("dream saved!");
 	});
+
+	//user那邊的Dreams關聯要更新
+	var query = new Parse.Query(User);
+	query.get(ownerID, {
+	  success: function(user) {
+	    /*//失敗做法
+	    var dreamsArr = [];
+		dreamsArr.push(dream);
+		user.add("Dreams" , dreamsArr);
+		user.save();*/
+		var relation = user.relation("Dreams");
+		relation.add(dream);
+		user.save().then(function(){
+			response.success("relation success");
+		});
+		/*//搜尋的方法 成功
+			relation.query().find({
+			success:function(dream){
+				response.success(dream[0].get("Dreams"));
+			}
+		});*/
+	  },
+	  error: function(object, error) {
+	    // error is a Parse.Error with an error code and description.
+	    response.success(error);
+	  }
+	});
+
 });
+
