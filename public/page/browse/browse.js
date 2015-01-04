e120f99d4283ae16dc934f10c2bce8cf1bdea7d9
jimmy <chioulf2@yahoo.com.tw>
2015-01-04 19:02:04 +0800

    把browse那頁的夢想動態連結 可以傳要跳過多少筆.要何種分類(string)

--- public/page/browse/browse.js
+++ public/page/browse/browse.js
@@ -1,14 +1,33 @@
-(function () {
-	fn.start = start;
-	
-	function start() {
-		var $paperArea = $('.paper-area');
-		var i, max = 7;
-		for(i = 0; i < max; i += 1) {
-			var $paper = $('<div class="paper"></div>');
-			
-			$paper.css({'left': Math.random()*80+"%", 'top': Math.random()*80+"%"});
-			$paperArea.append($paper);
+$(document).ready(function(){
+	getDream("all" , 0);
+});
+
+
+function getDream(type,skip){
+	var Dream = Parse.Object.extend("Dream");
+	var query = new Parse.Query(Dream);
+
+	//如果type不為空 or all 只搜尋那個型別
+	if(type != null && type != "all"){ query.equalTo("type" , type); }	
+	if(skip != null){ query.skip(skip); }
+
+	query.find({
+		success: function(results){
+			var $paperArea = $('.paper-area');
+			$paperArea.empty(); //先清空
+			var i, max = 7;
+			if(results.length < max){ max = results.length; } //如果不到最大筆數 就印他的比數
+
+			for(i = 0; i < max; i += 1) {
+				var result = results[i];
+				var $paper = $('<div class="paper">標題:'+result.get("title")+'<br/>內容:'+
+					result.get("description")+'</div>');		
+				$paper.css({'left': Math.random()*80+"%", 'top': Math.random()*80+"%"});
+				$paperArea.append($paper);
+			}
+		},
+		error: function(error){
+			alert("抓取夢想失敗，請再試一次"+" "+error.message);
 		}
-	}
-})()
+	});
+}
