1f81849dbc30ed33cfbcf57f68b3a33421a2f7bc
Bonerchick <jerry82422@gmail.com>
2015-01-05 00:48:57 +0800

    減少全域變數reference

--- public/core/js/init.js
+++ public/core/js/init.js
@@ -2,8 +2,8 @@
 	// Initialize Parse
 	Parse.initialize("XhswR68mEhMwVnswmQd5Tpkc09XSc6n6snwFm2Rn", "7jF1txHJlMP9bRT9wIUC2qiFwh6jB1ITYQDVoslt");
 	// 所有全域function
-	window.fn = {};
-	
+	var fn = window.funcs = {};
+
 	// Facebook initialized
 	fn.fbInitDone = fbInitDone;
 	
@@ -11,7 +11,7 @@
 		// 設定menu
 		setupMenu();
 		// 開始執行主要程式
-		fn.start();
+		if(fn && fn.start) fn.start();
 		
 		
 		function setupMenu () {
