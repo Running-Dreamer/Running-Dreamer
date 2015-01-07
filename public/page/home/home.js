be1b9a985922250533b88fa678abf2a656463636
jimmy <chioulf2@yahoo.com.tw>
2015-01-07 23:21:37 +0800

    新增modal半成品 上一版本衝突去掉

--- public/page/home/home.js
+++ public/page/home/home.js
@@ -3,6 +3,16 @@
 	fn.start = start;
 	
 	function start() {
-		
+		$(document).ready(function () {
+
+			$('.pencil').on('click', function(){
+				Modal.init();
+				Modal.show();
+			});
+					
+			$('#upload').on('click', function(){
+				Modal.hide();
+			});
+		});
 	}
 })()
