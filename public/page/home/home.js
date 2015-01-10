cc73edce69790090de7172829d9061a6511d8dc7
jimmy <chioulf2@yahoo.com.tw>
2015-01-10 17:41:26 +0800

    增加新增夢想時未選圖片的例外處理 按下新增夢想時先把按鈕鎖起來

--- public/page/home/home.js
+++ public/page/home/home.js
@@ -24,41 +24,50 @@
                 $(this).data('file', files[0]);
             });
             $('#upload').on('click', function () {
-                var parseFile = new Parse.File("photo", $('#photo').data('file'));
-                parseFile.save().then(function (file) {
-                    var Dream = Parse.Object.extend("Dream");
-                    var dream = new Dream();
-                    var User = Parse.Object.extend("User");
-                    var owner = new User();
-                    owner.id = Parse.User.current().id;
-                    dream.set("owner", owner);
-                    dream.set("title", $('#title').val());
-                    dream.set("description", $('#description').val());
-                    dream.set("photo", file);
-                    dream.set("type", $('#type').val());
-                    dream.save().then(function () {
-                        //user那邊的Dreams關聯要更新
-                        var query = new Parse.Query(User);
-                        query.get(owner.id, {
-                            success: function (user) {
-                                var relation = user.relation("Dreams");
-                                relation.add(dream);
-                                user.save().then(function () {
-                                    console.log("relation success");
-                                    alert("新增成功!");
-                                });
-                            },
-                            error: function (object, error) {
-                                // error is a Parse.Error with an error code and description.
-                                console.log(error);
-                                alert("新增失敗..");
-                            }
-                        });
-                        newDreamModal.hide();
-                        location.reload();
-                    });
+                $(this).attr('disabled' , 'disabled');
 
-                });
+                try{
+                    var parseFile = new Parse.File("photo", $('#photo').data('file'));
+                    parseFile.save().then(function (file) {
+                        var Dream = Parse.Object.extend("Dream");
+                        var dream = new Dream();
+                        var User = Parse.Object.extend("User");
+                        var owner = new User();
+                        owner.id = Parse.User.current().id;
+                        dream.set("owner", owner);
+                        dream.set("title", $('#title').val());
+                        dream.set("description", $('#description').val());
+                        dream.set("photo", file);
+                        dream.set("type", $('#type').val());
+                        dream.save().then(function () {
+                            //user那邊的Dreams關聯要更新
+                            var query = new Parse.Query(User);
+                            query.get(owner.id, {
+                                success: function (user) {
+                                    var relation = user.relation("Dreams");
+                                    relation.add(dream);
+                                    user.save().then(function () {
+                                        console.log("relation success");
+                                        alert("新增成功!");
+                                        location.reload();
+                                    });
+                                },
+                                error: function (object, error) {
+                                    // error is a Parse.Error with an error code and description.
+                                    console.log(error);
+                                    alert("新增失敗..");
+                                }
+                            });
+                            $(this).removeAttr('disabled');
+                            newDreamModal.hide();
+                        });
+
+                    });
+                }
+                catch(e){
+                    alert("請選擇照片後再上傳");
+                    $(this).removeAttr('disabled');
+                }
 
             });
             $('.detailBtn').on('click', function () {
