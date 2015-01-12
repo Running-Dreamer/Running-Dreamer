1eeed3356311d2b5095e582efa761e7d938432c0
tima59 <h4272752@yahoo.com.tw>
2015-01-13 01:35:36 +0800

    搜尋姓名完成(目前只能完全一樣的才搜尋到)

--- public/core/autocomplete/autocomplete.js
+++ public/core/autocomplete/autocomplete.js
@@ -1,15 +1,26 @@
         
 
 var countries = [
-    { value: 'Andorra', data: 'AD' },
-    { value: 'Zimbabwe', data: 'ZZ' }
+    { value2: 'Andorra', data2: 'AD' },
+    { value2: 'Zimbabwe', data2: 'ZZ' }
 ];
 
 $(document).ready(function() {
     $("#search").autocomplete({
-        lookup: countries,
+        serviceUrl: '/autocomplete',
+        //lookup: countries,
+        paramName: 'displayName',
+        transformResult: function(response) {
+            //alert(response);
+            return {
+                suggestions: $.map(JSON.parse(response), function(dataItem) {
+                    return { value: dataItem.displayName, data: dataItem.objectId };
+                })
+            };
+        },
         onSelect: function (suggestion) {
-            alert('You selected: ' + suggestion.value + ', ' + suggestion.data);
+            //alert('You selected: ' + suggestion.value + ', ' + suggestion.data);
+            location.replace("/other?UserId="+suggestion.data);
         }
     });
 });
